e29c8790a3eb026344f6f17ab0c26d2aba1074a4
George Drayson <drayson.g@mac.com>
2018-06-02 17:14:08 +0200

    Added navigation testing

---
+++ spec/features/navigation_testing_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Navigation", type: :feature do
+  scenario "Can visit the Blog from the homepage" do
+    visit "/"
+    click_link "Blog"
+    expect(page).to have_current_path("/articles")
+  end
+  scenario "Can visit Home from the blog page" do
+    visit "/articles"
+    click_link "Home"
+    expect(page).to have_current_path("/home/index")
+  end
+  scenario "Can click on an article to show it" do
+    sign_up
+    add_article
+    click_link "Back"
+    click_link "Example title"
+    expect(page).to have_content("Example title")
+    expect(page).to have_content("Example text")
+    expect(page).to have_content "Back"
+    expect(page).to have_content "Edit"
+  end
+  scenario "Can click on the edit button from the index page" do
+    sign_up
+    add_article
+    click_link "Back"
+    click_button "Edit"
+    expect(page).to have_content("Edit")
+  end
+end
