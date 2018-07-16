2c6b05bc538f29644fdc14844b7a1aa13827b92d
George Drayson <drayson.g@mac.com>
2018-07-16 20:07:57 +0100

    Added education level

---
+++ db/migrate/20180716185836_add_education_level_to_users.rb
@@ -0,0 +1,5 @@
+class AddEducationLevelToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :education_level, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716182736) do
+ActiveRecord::Schema.define(version: 20180716185836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180716182736) do
     t.integer "write_access"
     t.string "bio"
     t.string "location"
+    t.string "education_level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
