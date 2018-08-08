44be376a9bcb5be4ceae16dec2e895c927e30fd3
George Drayson <drayson.g@mac.com>
2018-08-08 15:43:21 +0100

    added profile images to users and changed profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716185836) do
+ActiveRecord::Schema.define(version: 20180718132028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20180716185836) do
     t.string "bio"
     t.string "location"
     t.string "education_level"
+    t.string "profile_img"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
